2f68e4a45d407307a43b4291de4d6d0ff095e40a
SabriDemir123 <sabri_demir@live.nl>
2024-05-16 14:58:40 +0200

    Added cliché building blocks

---
+++ genetic_algorithm/notes.py
@@ -0,0 +1,45 @@
+treble_clef_compositions = [
+    # Beethoven's fragments
+    (('e4', 8), ('f#4', 8), ('g4*', 4), ('f#4', 8), ('e4', 8), ('d#4*', 4), ('e4', 8), ('f#4', 8)),  # Beethoven's Symphony No. 5
+    (('g4', 8), ('f4', 8), ('e4', 8), ('d4', 8), ('c4', 8), ('b3', 8), ('a3', 8), ('g3', 8)),  # Beethoven's Für Elise
+    (('e4', 8), ('g#4', 8), ('b4', 8), ('e5', 8), ('b4', 8), ('g#4', 8), ('e4', 8), ('g#4', 8)),  # Beethoven's Moonlight Sonata
+    (('c4', 8), ('d4', 8), ('e4', 8), ('f4', 8), ('g4', 8), ('a4', 8), ('b4', 8), ('c5', 8)),  # Beethoven's Ode to Joy
+    # Mozart's fragments
+    (('g4', 8), ('a4', 8), ('b4', 8), ('a4', 8), ('g4', 8), ('f#4', 8), ('e4', 8), ('f#4', 8)),  # Mozart's Symphony No. 40
+    (('e4', 8), ('d4', 8), ('c4', 8), ('b3', 8), ('a3', 8), ('b3', 8), ('c4', 8), ('d4', 8)),  # Mozart's Eine kleine Nachtmusik
+    (('d4', 8), ('e4', 8), ('f#4', 8), ('g4', 8), ('a4', 8), ('b4', 8), ('c#5', 8), ('d5', 8)),  # Mozart's Turkish March
+    (('c4', 8), ('b3', 8), ('a3', 8), ('g3', 8), ('f3', 8), ('e3', 8), ('d3', 8), ('c3', 8)),  # Mozart's Symphony No. 25
+    # Bach's fragments
+    (('f4', 8), ('e4', 8), ('d4', 8), ('c4', 8), ('b3', 8), ('c4', 8), ('d4', 8), ('e4', 8)),  # Bach's Air on the G String
+    (('d4', 8), ('f#4', 8), ('a4', 8), ('d5', 8), ('a4', 8), ('f#4', 8), ('d4', 8), ('f#4', 8)),  # Bach's Brandenburg Concerto No. 3
+    (('g4', 8), ('b4', 8), ('d5', 8), ('f5', 8), ('d5', 8), ('b4', 8), ('g4', 8), ('b4', 8)),  # Bach's Jesu, Joy of Man's Desiring
+    (('f4', 8), ('e4', 8), ('d4', 8), ('c4', 8), ('b3', 8), ('a3', 8), ('g#3', 8), ('a3', 8)),  # Bach's Toccata and Fugue in D minor
+    # Chopin's fragments
+    (('c#4', 8), ('e4', 8), ('g#4', 8), ('c#5', 8), ('g#4', 8), ('e4', 8), ('c#4', 8), ('e4', 8)),  # Chopin's Revolutionary Étude
+    (('e4', 8), ('g#4', 8), ('b4', 8), ('e5', 8), ('b4', 8), ('g#4', 8), ('e4', 8), ('g#4', 8)),  # Chopin's Nocturne Op. 9 No. 2
+    (('c#3', 8), ('e3', 8), ('g#3', 8), ('c#4', 8), ('g#3', 8), ('e3', 8), ('c#3', 8), ('e3', 8)),  # Chopin's Étude Op. 10 No. 4
+    (('e5', 8), ('g#5', 8), ('b5', 8), ('e6', 8), ('b5', 8), ('g#5', 8), ('e5', 8), ('g#5', 8)),   # Chopin's Ballade No. 1 in G minor
+]
+
+bass_clef_compositions = [
+    # Beethoven's fragments
+    (('e3', 8), ('d3', 8), ('c3', 8), ('b2', 8), ('a2', 8), ('b2', 8), ('c3', 8), ('d3', 8)),  # Beethoven's Für Elise
+    (('c3', 8), ('d3', 8), ('e3', 8), ('f3', 8), ('g3', 8), ('a3', 8), ('b3', 8), ('c4', 8)),  # Beethoven's Ode to Joy
+    (('e3', 8), ('g#3', 8), ('b3', 8), ('e4', 8), ('b3', 8), ('g#3', 8), ('e3', 8), ('g#3', 8)),  # Beethoven's Moonlight Sonata
+    (('c3', 8), ('d3', 8), ('e3', 8), ('f3', 8), ('g3', 8), ('a3', 8), ('b3', 8), ('c4', 8)),  # Beethoven's Ode to Joy
+    # Mozart's fragments
+    (('g2', 8), ('a2', 8), ('b2', 8), ('a2', 8), ('g2', 8), ('f2', 8), ('e2', 8), ('f2', 8)),  # Mozart's Symphony No. 25
+    (('c3', 8), ('b2', 8), ('a2', 8), ('g2', 8), ('f2', 8), ('g2', 8), ('a2', 8), ('b2', 8)),  # Mozart's Eine kleine Nachtmusik
+    (('d2', 8), ('e2', 8), ('f#2', 8), ('g2', 8), ('a2', 8), ('b2', 8), ('c#3', 8), ('d3', 8)),  # Mozart's Turkish March
+    (('c2', 8), ('b1', 8), ('a1', 8), ('g1', 8), ('f1', 8), ('e1', 8), ('d1', 8), ('c1', 8)),  # Mozart's Symphony No. 25
+    # Bach's fragments
+    (('f2', 8), ('e2', 8), ('d2', 8), ('c2', 8), ('b1', 8), ('c2', 8), ('d2', 8), ('e2', 8)),  # Bach's Air on the G String
+    (('d2', 8), ('f#2', 8), ('a2', 8), ('d3', 8), ('a2', 8), ('f#2', 8), ('d2', 8), ('f#2', 8)),  # Bach's Brandenburg Concerto No. 3
+    (('g2', 8), ('b2', 8), ('d3', 8), ('f3', 8), ('d3', 8), ('b2', 8), ('g2', 8), ('b2', 8)),  # Bach's Jesu, Joy of Man's Desiring
+    (('f2', 8), ('e2', 8), ('d2', 8), ('c2', 8), ('b1', 8), ('a1', 8), ('g#1', 8), ('a1', 8)),  # Bach's Toccata and Fugue in D minor
+    # Chopin's fragments
+    (('c#2', 8), ('e2', 8), ('g#2', 8), ('c#3', 8), ('g#2', 8), ('e2', 8), ('c#2', 8), ('e2', 8)),  # Chopin's Revolutionary Étude
+    (('e2', 8), ('g#2', 8), ('b2', 8), ('e3', 8), ('b2', 8), ('g#2', 8), ('e2', 8), ('g#2', 8)),  # Chopin's Nocturne Op. 9 No. 2
+    (('c#3', 8), ('e3', 8), ('g#3', 8), ('c#4', 8), ('g#3', 8), ('e3', 8), ('c#3', 8), ('e3', 8)),  # Chopin's Étude Op. 10 No. 4
+    (('e3', 8), ('g#3', 8), ('b3', 8), ('e4', 8), ('b3', 8), ('g#3', 8), ('e3', 8), ('g#3', 8)),  # Chopin's Ballade No. 1 in G minor
+]
